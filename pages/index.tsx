ac3c426fa71c35d425f685171b57e9fc57d6625b
HRTK92 <tkh102692@gmail.com>
2022-04-28 11:39:14 +0000

    update

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <div>aaa</div>
+    </>
+  )
+}
+
+export default Home
